0e9348822e2b8d7b7a1770075ca20dc9a7dc564d
Adiesha <adiesha@gmail.com>
2020-10-26 14:40:50 -0600

    added false alarm rate metric

---
+++ assessments.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+
+
+def falsealarmrate(olabels, oelement, clabels, celement):
+    N = np.count_nonzero(olabels == oelement)
+    negativeOarray = (olabels == oelement)
+    correspondingClabelarr = clabels[negativeOarray]
+    fp = len(correspondingClabelarr) - np.count_nonzero(correspondingClabelarr == celement)
+    result = fp/N
+    print(result)
+    return result
+
+
+def main():
+    pass
+
+
+def test():
+    alabels = np.array(['n', 'n', 'n', 'n', 'n', 'o', 'n', 'n', 'n', 'n', 'o', 'n', 'n', 'n', 'n', 'o'])
+    clabels = np.array([0, 0, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, 0, 0, 0])
+    print(alabels)
+    print(clabels)
+    print(np.count_nonzero(alabels == 'o'))
+
+    boolarray = alabels == 'o'
+    print(boolarray)
+    newarr = clabels[boolarray]
+    print(newarr)
+    fp = len(newarr) - np.count_nonzero(newarr == -1)
+    print(fp)
+
+    falsealarmrate(alabels, 'o', clabels, -1)
+if __name__ == '__main__':
+    test()
