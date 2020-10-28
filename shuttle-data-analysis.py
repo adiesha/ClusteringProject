23209079ae54b7c41b16e511493a60b2a4e5878b
Adiesha <adiesha@gmail.com>
2020-10-28 17:58:49 -0600

    changed shuttle analysis script

--- shuttle-data-analysis.py
+++ shuttle-data-analysis.py
@@ -6,19 +6,21 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    data = pd.read_csv('data/shuttle-unsupervised-trn.csv', sep=' ', header=None)
+    data = pd.read_csv('data/shuttle-unsupervised-trn.csv', header=None)
     print(data.head())
+    print(data.describe())
     y = data[9]
     y.hist()
     plt.show()
 
     # classes 2, 3, 6, 7 has the least number of frequency. Thus we will use those classes as the outliers.
 
-    clustering = DBSCAN(eps=42, min_samples=5).fit(data.iloc[:, 0:8])
+    clustering = DBSCAN(eps=5, min_samples=6).fit(data.iloc[:, 0:8])
     print(clustering.labels_)
+    print(type(clustering.labels_))
     # print(type(clustering.labels_))
     for index, row in data.iterrows():
-        print(row[36] + " " + str(clustering.labels_[index]))
+        print(str(row[9]) + ":" + str(clustering.labels_[index]))
 
     # print('false alarm rate: ', metrics.falsealarmrate(data.iloc[:, 36].values, 'o', clustering.labels_, -1))
 
