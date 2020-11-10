3809d9198abe40fd7f9d99513ae0987012100055
Adiesha <adiesha@gmail.com>
2020-11-10 16:01:31 -0700

    added multiple noise point list for the false alarm rate calculation

--- assessments.py
+++ assessments.py
@@ -11,8 +11,25 @@ def falsealarmrate(olabels, oelement, clabels, celement):
     return result
 
 
+def falsealarmrate(olabels, oelement, clabels, celement):
+    negativeOarray = np.full(len(olabels), False)
+    for oe in oelement:
+        negativeOarray = np.bitwise_or((olabels == oe), negativeOarray)
+    N = np.count_nonzero(negativeOarray)  # count the negatives in ground truth
+    correspondingClabelarr = clabels[negativeOarray]  # create the expected negative cluster label array
+    fp = len(correspondingClabelarr) - np.count_nonzero(correspondingClabelarr == celement)  # count the false positives
+    result = N if N == 0 else fp / N
+    return result
+
+
 def main():
     pass
+
+
+def test2():
+    alabels = np.array(['n', 'u', 'n', 'n', 'n', 'o', 'n', 'n', 'n', 'n', 'o', 'n', 'n', 'u', 'n', 'o'])
+    clabels = np.array([0, -1, 0, 0, 0, -1, 0, 0, 0, 0, -1, 0, 0, -1, 0, 0])
+    print(falsealarmrate(alabels, ['o', 'u'], clabels, -1))
 
 
 def test():
@@ -29,8 +46,9 @@ def test():
     fp = len(newarr) - np.count_nonzero(newarr == -1)
     print(fp)
 
-    falsealarmrate(alabels, 'o', clabels, -1)
+    print(falsealarmrate(alabels, 'o', clabels, -1))
 
 
 if __name__ == '__main__':
     test()
+    test2()
