c1560b635254312be595ba078e652b445c01c870
Adiesha <adiesha@gmail.com>
2020-11-09 20:19:06 -0700

    added satellite histogram

--- satellite-data-analysis.py
+++ satellite-data-analysis.py
@@ -2,10 +2,14 @@ import numpy as np
 import pandas as pd
 from sklearn.cluster import DBSCAN
 import assessments as metrics
+import matplotlib.pyplot as plt
 
 def main():
     data = pd.read_csv('data/satellite-unsupervised-ad.csv', header=None)
     print(data.head())
+    y = data[36]
+    y.hist()
+    plt.show()
     clustering = DBSCAN(eps=42, min_samples=7).fit(data.iloc[:, 0:35])
     print()
     print(clustering.labels_)
